979c21f030c760f1514422fd470e6f3b73bf66ff
Swag-blips <coderblip@gmail.com>
2025-06-03 20:57:52 +0100

    update:login page

--- src/app/(auth)/auth/page.tsx
+++ src/app/(auth)/auth/page.tsx
@@ -2,19 +2,28 @@ import Image from "next/image";
 
 const AuthPage = () => {
   return (
-    <main className="grid w-full grid-cols-2">
-      <section className="flex   items-center justify-center">
-        <h2>Hello</h2>
+    <main className="grid grid-cols-2">
+      <section className="flex   flex-col flex-1  items-center justify-center">
+        <h2 className="font-medium text-3xl">Get started</h2>
+
+        <div className="bg-[#F0EFF2] w-[381px] pl-1 mt-10 rounded-lg h-14 flex items-center justify-between">
+          <button className="bg-white w-[169px]  py-3  my-[0.5px] font-medium rounded-lg">
+            signup
+          </button>
+          <button className=" w-[169px]  py-3  my-[0.5px] font-medium rounded-lg text-[#868686]">
+            login
+          </button>
+        </div>
+
+        <form className="flex flex-col  gap-6"></form>
       </section>
 
-      <section className=" border h-screen relative border-red-500 ">
-        <div className="w-[433px] ">
-          <Image
+      <section className=" ml-auto w-fit h-screen pl-auto relative ">
+        <div className=" h-[inherit]">
+          <img
             src="https://res.cloudinary.com/dh3c9ay9z/image/upload/v1748957920/Banner_ji5s0q.png"
             alt="Banner"
-            fill={true}
-            objectFit="contain"
-            // className="w-full  h-full"
+            className=" h-full"
           />
         </div>
       </section>
