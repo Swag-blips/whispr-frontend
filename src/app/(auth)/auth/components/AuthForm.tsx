c3f973dd87b73964b4e34aa2e7a0759b45028fa9
Swag-blips <coderblip@gmail.com>
2025-06-04 09:37:54 +0100

    feat:setup authform

---
+++ src/app/(auth)/auth/components/AuthForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useState } from "react";
+
+type ActiveTab = "signup" | "login";
+export const AuthForm = () => {
+  const [activeTab, setActiveTab] = useState<ActiveTab>("signup");
+  return (
+    <>
+      <div className="bg-[#F0EFF2] w-[390px] px-1 mt-10 rounded-lg h-14 flex items-center justify-between">
+        <button
+          onClick={() => setActiveTab("signup")}
+          className={` ${
+            activeTab === "signup" ? "bg-white" : "text-[#868686]"
+          }  cursor-pointer w-[169px]  py-3  my-[0.5px] font-medium rounded-lg`}
+        >
+          signup
+        </button>
+        <button
+          onClick={() => setActiveTab("login")}
+          className={` w-[169px] ${  
+            activeTab === "login" ? "bg-white" : "text-[#868686]"
+          } cursor-pointer py-3  my-[0.5px] font-medium rounded-lg `}
+        >
+          login
+        </button>
+      </div>
+      <form className="flex flex-col mt-8   gap-6">
+        <div className="flex items-center gap-6">
+          <div className="flex flex-col gap-2 ">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="border-[#D9D9D9] outline-[#444CE7] placeholder:text-[#C4C4C4] placeholder-text-sm border-[0.5px] rounded-lg py-3 pl-3"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="flex flex-col gap-2 ">
+            <label htmlFor="bio">Bio(optional)</label>
+            <input
+              type="bio"
+              id="bio"
+              name="bio"
+              className="border-[#D9D9D9] outline-[#444CE7] placeholder-font-normal placeholder:text-[#C4C4C4] placeholder-text-sm border-[0.5px] rounded-lg py-3 pl-3"
+              placeholder="Enter your bio"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2 ">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="border-[#D9D9D9] outline-[#444CE7] placeholder:text-[#C4C4C4] placeholder-text-sm border-[0.5px] rounded-lg py-3 pl-3"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="flex flex-col gap-2 ">
+          <label htmlFor="email">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="border-[#D9D9D9] outline-[#444CE7] placeholder:text-[#C4C4C4] placeholder-text-sm border-[0.5px] rounded-lg py-3 pl-3"
+            placeholder="Enter your password"
+          />
+        </div>
+
+        <button className="bg-[#444CE7] cursor-pointer text-center font-medium rounded-lg text-white py-4">
+          Get started
+        </button>
+      </form>
+    </>
+  );
+};
--- src/app/(auth)/auth/page.tsx
+++ src/app/(auth)/auth/page.tsx
@@ -2,6 +2,7 @@ import Apple from "@/app/components/icons/Apple";
 import Facebook from "@/app/components/icons/Facebook";
 import Google from "@/app/components/icons/Google";
 import Image from "next/image";
+import { AuthForm } from "./components/AuthForm";
 
 const AuthPage = () => {
   return (
@@ -9,64 +10,7 @@ const AuthPage = () => {
       <section className="flex   flex-col flex-1  items-center justify-center">
         <h2 className="font-medium text-3xl">Get started</h2>
 
-        <div className="bg-[#F0EFF2] w-[390px] pl-1 mt-10 rounded-lg h-14 flex items-center justify-between">
-          <button className="bg-white cursor-pointer w-[169px]  py-3  my-[0.5px] font-medium rounded-lg">
-            signup
-          </button>
-          <button className=" w-[169px] cursor-pointer py-3  my-[0.5px] font-medium rounded-lg text-[#868686]">
-            login
-          </button>
-        </div>
-
-        <form className="flex flex-col mt-8   gap-6">
-          <div className="flex items-center gap-6">
-            <div className="flex flex-col gap-2 ">
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                className="border-[#D9D9D9] outline-[#444CE7] placeholder:text-[#C4C4C4] placeholder-text-sm border-[0.5px] rounded-lg py-3 pl-3"
-                placeholder="Enter your username"
-              />
-            </div>
-            <div className="flex flex-col gap-2 ">
-              <label htmlFor="bio">Bio</label>
-              <input
-                type="bio"
-                id="bio"
-                name="bio"
-                className="border-[#D9D9D9] outline-[#444CE7] placeholder-font-normal placeholder:text-[#C4C4C4] placeholder-text-sm border-[0.5px] rounded-lg py-3 pl-3"
-                placeholder="Enter your bio"
-              />
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-2 ">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="border-[#D9D9D9] outline-[#444CE7] placeholder:text-[#C4C4C4] placeholder-text-sm border-[0.5px] rounded-lg py-3 pl-3"
-              placeholder="Enter your email"
-            />
-          </div>
-          <div className="flex flex-col gap-2 ">
-            <label htmlFor="email">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="border-[#D9D9D9] outline-[#444CE7] placeholder:text-[#C4C4C4] placeholder-text-sm border-[0.5px] rounded-lg py-3 pl-3"
-              placeholder="Enter your password"
-            />
-          </div>
-
-          <button className="bg-[#444CE7] cursor-pointer text-center font-medium rounded-lg text-white py-4">
-            Get started
-          </button>
-        </form>
+        <AuthForm />
 
         <div className="flex items-center justify-center mt-6 w-[390px] gap-2">
           <hr className="border-[#C4C4C4] border w-[117px]" />
