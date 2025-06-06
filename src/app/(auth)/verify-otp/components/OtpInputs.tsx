3794dcc3069f6bbe93f1f7afe7426f65456fe73c
Swag-blips <coderblip@gmail.com>
2025-06-06 15:09:37 +0100

    feat:otp input

---
+++ src/app/(auth)/verify-otp/components/OtpInputs.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+
+const OtpInputs = () => {
+  const [otp, setOtp] = useState(new Array(5).fill(""));
+  const [otpError, setOtpError] = useState<string | null>(null);
+  const [enabled, setEnabled] = useState(false);
+  const otpBoxReference = useRef<HTMLInputElement[]>([]);
+  const correctOtp = "12345";
+
+  const handleChange = (value: string, index: number) => {
+    const parsedValue = parseInt(value);
+    if (parsedValue > 9) return;
+    let newArr = [...otp];
+    newArr[index] = value;
+    setOtp(newArr);
+
+    if (value && index < 5 - 1) {
+      otpBoxReference.current[index + 1].focus();
+    }
+  };
+
+  const handleChangeCharacter = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    if (e.key === "Backspace" && !e.currentTarget.value && index > 0) {
+      otpBoxReference.current[index - 1].focus();
+    }
+    if (e.key === "Enter" && e.currentTarget.value && index < 5 - 1) {
+      otpBoxReference.current[index + 1].focus();
+    }
+  };
+
+  useEffect(() => {
+    if (otp.some((digit) => digit === "")) {
+      setEnabled(false);
+    } else {
+      setEnabled(true);
+    }
+    if (otp.join("") !== "" && otp.join("") !== correctOtp) {
+      setOtpError("❌ Wrong OTP Please Check Again");
+    } else {
+      setOtpError(null);
+    }
+  }, [otp]);
+
+  console.log("otp", otp);
+
+  return (
+    <div className="mt-10">
+      <div className="flex items-center gap-4">
+        {otp.map((value: number, index: number) => (
+          <input
+            value={value}
+            onChange={(e) => handleChange(e.target.value, index)}
+            onKeyUp={(e) => handleChangeCharacter(e, index)}
+            id={`otp-${index}`}
+            key={index}
+            type="number"
+            min="0"
+            max="9"
+            step="1"
+            ref={(el) => {
+              if (el) {
+                otpBoxReference.current[index] = el;
+              }
+            }}
+            className="outline-none focus:border-[#444CE7] text-center border rounded-lg border-[#868686] h-16 w-[71px] [&::-webkit-inner-spin-button]:appearance-none"
+          />
+        ))}
+      </div>
+      <button
+        disabled={!enabled}
+        className={`mt-10 h-14 rounded-lg ${
+          enabled ? "bg-[#444CE7] " : "bg-[#C4C4C4]"
+        } text-white font-medium disabled:cursor-not-allowed  w-full`}
+      >
+        Verify
+      </button>
+      <span className="underline flex items-center justify-center mt-8 text-[#444CE7]">
+        Resend otp
+      </span>
+    </div>
+  );
+};
+
+export default OtpInputs;
--- src/app/(auth)/verify-otp/page.tsx
+++ src/app/(auth)/verify-otp/page.tsx
@@ -1,10 +1,27 @@
+import { ArrowLeft } from "lucide-react";
 import React from "react";
+import OtpInputs from "./components/OtpInputs";
 
-type Props = {};
+const OtpPage = () => {
+  return (
+    <div className="">
+      <nav className="py-4 px-3">
+        <ArrowLeft />
+      </nav>
+      <main className="flex items-center justify-center h-[calc(100vh-64px)]  flex-col gap-2">
+        <h2 className=" text-[32px] font-bold text-center ">
+          Enter your otp code
+        </h2>
+        <p className="text-[#868686] text-center">
+          We sent an email to{" "}
+          <span className="text-[#444CE7]">coderblip@gmail.com</span> enter it
+          below to continue
+        </p>
 
-const OtpPage = (props: Props) => {
-  return <div>page</div>;
+        <OtpInputs />
+      </main>
+    </div>
+  );
 };
 
 export default OtpPage;
-       
