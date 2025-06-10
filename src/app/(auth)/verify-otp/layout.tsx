de1cbf80602361d22f6734315acf5f3b6ad74156
Turbobot <turbobot@vercel.com>
2025-06-10 20:43:33 +0100

    feat(create-turbo): apply official-starter transform

--- src/app/(auth)/verify-otp/layout.tsx
+++
@@ -1,5 +0,0 @@
-const OtpLayoutPage = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
-};
-
-export default OtpLayoutPage;
--- src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const ApiConfig = {
-  url: process.env.NEXT_PUBLIC_API_URL,
-};
