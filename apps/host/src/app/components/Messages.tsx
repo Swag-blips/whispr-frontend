f7872973bdc42e0999aba2c475e00ed0bdc91a07
Swag-blips <coderblip@gmail.com>
2025-06-19 23:26:21 +0100

    feat:messaging feature integrate

--- apps/host/src/app/components/Messages.tsx
+++ apps/host/src/app/components/Messages.tsx
@@ -13,52 +13,56 @@ import { convertTime } from "../utils/convertDate";
 export const Messages = () => {
   const { currentChat } = useChatStore();
   const { user } = useAuth();
-  const { data } = useSWR(currentChat?._id, getMessages);
+  const { data, isLoading, error } = useSWR(currentChat?._id, getMessages);
 
-  console.log("messages here", data);
-  console.log("Users data", user);
-  console.log(data?.messages[0].senderId === user?._id);
+  if (isLoading) return <p>loading....</p>;
+  if (error) return <p>{error}</p>;
+
   return (
     <div className="flex flex-col h-full overflow-y-auto px-4 py-8">
       <div className="flex-col pb-[90px] flex gap-6">
-        {data?.messages.map((msg) => (
-          <div
-            key={msg._id}
-            className={`flex  ${msg.senderId === user?._id ? "ml-auto flex-row-reverse" : ""} items-start gap-2`}
-          >
-            <Image
-              src={
-                msg.senderId === user?._id
-                  ? getAvatar(user.avatar)
-                  : getAvatar(msg.otherUserDetails.avatar)
-              }
-              alt={"user"}
-              width={48}
-              height={48}
-              className="rounded-full"
-            />
+        {data?.messages && data?.messages.length > 0 ? (
+          data?.messages.map((msg) => (
+            <div
+              key={msg._id}  
+              className={`flex  ${msg.senderId === user?._id ? "ml-auto flex-row-reverse" : ""} items-start gap-2`}
+            >
+              <Image
+                src={
+                  msg.senderId === user?._id
+                    ? getAvatar(user.avatar)
+                    : getAvatar(msg.otherUserDetails.avatar)
+                }
+                alt={"user"}
+                width={48}
+                height={48}
+                className="rounded-full"
+              />
 
-            <div className="flex flex-col gap-2">
-              <div
-                className={`flex ${msg.senderId === user?._id ? "flex-row-reverse" : ""} items-center gap-4`}
-              >
-                <h2 className="font-medium">
-                  {msg.senderId === user?._id
-                    ? user.username
-                    : msg?.otherUserDetails.username}
-                </h2>
-                <p className="text-[#8C8C8C] text-sm"> 
-                  {convertTime(msg.createdAt)}
+              <div className="flex flex-col gap-2">
+                <div
+                  className={`flex ${msg.senderId === user?._id ? "flex-row-reverse" : ""} items-center gap-4`}
+                >
+                  <h2 className="font-medium">
+                    {msg.senderId === user?._id
+                      ? user.username
+                      : msg?.otherUserDetails.username}
+                  </h2>
+                  <p className="text-[#8C8C8C] text-sm">
+                    {convertTime(msg.createdAt)}
+                  </p>
+                </div>
+                <p
+                  className={` ${msg.senderId === user?._id ? "bg-[#444CE7] text-white ml-auto" : "bg-white text-black"} w-fit rounded-tr-lg rounded-br-lg rounded-bl-lg  p-4 `}
+                >
+                  {msg.content}
                 </p>
-              </div> 
-              <p
-                className={` ${msg.senderId === user?._id ? "bg-[#444CE7] text-white ml-auto" : "bg-white text-black"} w-fit rounded-tr-lg rounded-br-lg rounded-bl-lg  p-4 `}
-              >
-                {msg.content}
-              </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <div>Start your Conversation</div>
+        )}
       </div>
     </div>
   );
