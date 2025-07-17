4cfb6d7efaed66e4c24057356542e7248efb2942
Swag-blips <coderblip@gmail.com>
2025-07-17 16:23:54 +0100

    fix:next config

--- apps/host/next.config.ts
+++ apps/host/next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "lh3.googleusercontent.com",
+        hostname: "lh3.googleusercontent.com", 
       },
     ],
   },
